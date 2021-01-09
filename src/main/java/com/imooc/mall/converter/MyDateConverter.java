184949709ee5455517bfa8996d081b7c1dc5ddf6
YIBOWAY <2338129627@qq.com>
2021-01-09 20:55:09 +0800

    1.添加时间转换器类，若是同时有时间转换器类及@RequestDateFormat注解时，按照时间转换器类来进行转换； 2.实际应用的时间转换器类应该包含多种对传入参数格式的判断，将其转换成不同格式；

---
+++ src/main/java/com/imooc/mall/converter/MyDateConverter.java
@@ -0,0 +1,20 @@
+package com.imooc.mall.converter;
+
+import org.springframework.core.convert.converter.Converter;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MyDateConverter implements Converter<String, Date> {
+    @Override
+    public Date convert(String s) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            Date d = sdf.parse(s);
+            return d;
+        } catch (ParseException e) {
+            return null;
+        }
+    }
+}
